30040a56f1269b13593979f2fbbaafed0c177273
Williann008 <89262997+Williann008@users.noreply.github.com>
2023-12-15 13:01:17 -0500

    Update gameflow.cs
    fixed ifs

--- Assets/gameflow.cs
+++ Assets/gameflow.cs
@@ -44,46 +44,46 @@ public class gameflow : MonoBehaviour
 
         for(int i = 0; i < 3; i++)
         {
-            if(orderValue[i] == 11001)
+            if (orderValue[i] == 11001)
             {
-                    currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[0];
-                }
-            
-            if(orderValue[i] == 11011)
-            {
-                    currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[1];
-                }
+                currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[0];
+            }
 
-            if(orderValue[i] == 11101)
+            if (orderValue[i] == 11011)
             {
-                    currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[2];
-                }
+                currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[1];
+            }
 
-            if(orderValue[i] == 11111)
+            if (orderValue[i] == 11101)
             {
-                    currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[3];
-                }
+                currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[2];
+            }
 
-            if(orderValue[i] == 12001)
+            if (orderValue[i] == 11111)
             {
-                    currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[4];
-                }
+                currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[3];
+            }
 
-            if(orderValue[i] == 12011)
+            if (orderValue[i] == 12001)
             {
-                    currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[5];
-                }
+                currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[4];
+            }
 
-            if(orderValue[i] == 12101)
+            if (orderValue[i] == 12011)
             {
-                    currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[6];
-                }
+                currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[5];
+            }
 
-            if(orderValue[i] == 12111)
+            if (orderValue[i] == 12101)
             {
-                    currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[7];
-                }
+                currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[6];
+            }
 
+            if (orderValue[i] != 12111)
+            {
+                 continue;
+            }
+            currentPic[i].GetComponent<MeshRenderer>().material.mainTexture = orderPics[7];
             }
         }
 
