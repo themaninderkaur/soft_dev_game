14dcee4949b680e031aa759e5515a1b664d7c051
Maninder Kaur <75804314+themaninderkaur@users.noreply.github.com>
2023-12-18 16:09:57 -0500

    Update meat.cs

--- Assets/meat.cs
+++ Assets/meat.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class meat : MonoBehaviour
 {
+
+    /*Meat.cs is to insatntiate the patty. The cloneObj
+    clones the icon of the patty and moves it to the grill.*/
     public Transform cloneObj;
 
     void Start()
@@ -16,11 +19,13 @@ public class meat : MonoBehaviour
         
     }
 
+    /*If the icon pressed is the patty cutlet, 
+    the object is created on the grill. */
     private void OnMouseDown()
     {
         if(gameObject.name == "Cutlet")
         {
-            Instantiate(cloneObj, new Vector3(-3, -.1f), cloneObj.rotation);
+            Instantiate(cloneObj, new Vector2(-3, -.1f), cloneObj.rotation);
         }
     }
 }
